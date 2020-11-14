33288ae1f27266008c36712fe92aed28202e5831
Johan Thomsen <jth@dbc.dk>
2020-11-14 11:46:09 +0100

    inherit package pin

--- default.nix
+++ default.nix
@@ -9,4 +9,5 @@ in
   nyancat = import ./images/nyancat.nix { inherit pkgs; };
   sl = import ./images/sl.nix { inherit pkgs; };
   toolbox = import ./images/toolbox.nix { inherit pkgs; };
+  inherit pkgs;
 }
