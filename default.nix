5d79d0cb0419bc52e5069f23734ccd47ce3e9ec3
Johan Thomsen <jth@dbc.dk>
2020-11-14 11:53:40 +0100

    reset nixpkgs config

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   pkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/3090c65041104931adda7625d37fa874b2b5c124.tar.gz";
     sha256 = "0r0gfsvvh69cy39izrgqhqlj26hxdqf37n3is6b6pma75k97axsj";
-  }) {};
+  }) { config = {}; };
 in
 {
   entryscript = import ./images/entryscript.nix { inherit pkgs; };
