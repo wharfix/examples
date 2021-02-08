ac6516d1888c31f39626cdf6a130bc0725d5312b
Johan Thomsen <jth@dbc.dk>
2021-02-08 13:35:20 +0100

    repo index _must_ be a function now

--- default.nix
+++ default.nix
@@ -1,9 +1,10 @@
 let
-  pkgs = import (builtins.fetchTarball {
+  _pkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/3090c65041104931adda7625d37fa874b2b5c124.tar.gz";
     sha256 = "0r0gfsvvh69cy39izrgqhqlj26hxdqf37n3is6b6pma75k97axsj";
   }) { config = {}; };
 in
+{ pkgs ? _pkgs, ... }:
 {
   entryscript = import ./images/entryscript.nix { inherit pkgs; };
   nyancat = import ./images/nyancat.nix { inherit pkgs; };
